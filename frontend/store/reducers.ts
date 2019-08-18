ffe07d6158ee52b79cfbc59e2f21747b35c1199e
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-08-18 18:34:16 +0800

    ignore bundles folder

--- frontend/store/reducers.ts
+++ frontend/store/reducers.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import { IInitialState } from "@store/states";
 import userReducers from "@store/user/reducers";
+import likeReducers from "./like/reducers";
 
 export const combinedReducers = combineReducers<IInitialState>({
   user: userReducers,
+  like: likeReducers,
 });
